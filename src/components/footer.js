553c8add4b1ec8e28259f3e19b620b58d44cad09
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-06-21 13:43:09 -0400

    Added background image and started header/footer components

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+//css
+import '../styles/footer.css'
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h1>FOOTER</h1>
+
+        </div>
+    )
+}
+
+export default Footer;
+
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Hi, my name is Ivan Morales</h1>
+            <h1>Welcome to my site</h1>
+        </div>
+    )
+}
+
+export default Header;
