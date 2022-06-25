5d47a6383a1f71ea7296312ec403d633b086c04c
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-06-24 20:32:50 -0400

    Added particles to background

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,15 @@ import '../styles/index.css'
 //Components
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Particle from '../components/Particle.js'
 
 const IndexPage = () => {
   return (
     <div className="home">
       <Header />
+      <Particle />
       <Footer />
+
     </div>
   )
 }
